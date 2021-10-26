b19e9f2de91be5c1491a6eab61f2d6304dff6b6c
ashu singh <shu.ask.singh@gmail.com>
2021-10-26 16:44:39 +0530

    Dependency Injection (Baseball Coach)

--- src/com/luv2code/springdemo/BaseballCoach.java
+++ src/com/luv2code/springdemo/BaseballCoach.java
@@ -2,8 +2,19 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 
+    private FortuneService theFortuneService;
+
+    public BaseballCoach(FortuneService theFortuneService) {
+        this.theFortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout(){
         return "Spend 30 mins on batting";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return theFortuneService.getFortune();
     }
 }
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -2,8 +2,15 @@ package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
 
+
+
     @Override
     public String getDailyWorkout() {
         return "Run 5K";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
     }
 }
